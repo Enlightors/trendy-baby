857d5e677724439bcada6362e0a3ea9e833795de
Heshwbahrooz <96142778+Heshwbahrooz@users.noreply.github.com>
2024-10-24 16:29:48 +0300

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,60 +1,51 @@
-import React from 'react'
-import { FaInstagram, FaFacebookF, FaYoutube, FaTiktok } from 'react-icons/fa';
+import React from "react";
+import { FaInstagram, FaFacebookF, FaYoutube, FaTiktok } from "react-icons/fa";
 
-export default function Footer() {
+export default function Footer({ locale }: any) {
   return (
-    <div className='flex flex-col bg-[#5ac5f1] h-[370px] text-white'>
-    <div className='grid grid-cols-3  px-10  pt-10 text-sm'>
-      <div className='flex flex-col gap-y-2 '>
-        <h1 className='font-medium text-4xl '> Contact</h1>
-        <p> Customer Service</p>
-        <p>Monday to Friday</p>
-        <p>From 9am to 5pm</p>
-        <p className='font-semibold'>babybrezza@onnaworld.com</p>
+    <div className="flex flex-col bg-[#5ac5f1] h-[370px] text-white">
+      <div className="grid grid-cols-3  px-10  pt-10 text-sm">
+        <div className="flex flex-col gap-y-2 ">
+          <h1 className="font-medium text-4xl "> Contact</h1>
+          <p> Customer Service</p>
+          <p>Monday to Friday</p>
+          <p>From 9am to 5pm</p>
+          <p className="font-semibold">babybrezza@onnaworld.com</p>
+        </div>
+        <div className="flex flex-col gap-y-2 py-12">
+          <p>Home</p>
+          <p>About US</p>
+          <p>Customer Services</p>
+          <p>Products</p>
+          <p>Spaire Parts</p>
+
+          <p>Blog</p>
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <h1 className="font-medium text-4xl "> Socials</h1>
+          <span className="bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center">
+            <a href="" target="_blank" rel="noreferrer">
+              <FaFacebookF color="#5ac5f1" />
+            </a>
+          </span>
+
+          <span className="bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center">
+            <a href="" target="_blank" rel="noreferrer">
+              <FaInstagram color="#5ac5f1" />
+            </a>
+          </span>
+
+          <span className="bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center">
+            <a href="" target="_blank" rel="noreferrer">
+              <FaTiktok color="#5ac5f1" />
+            </a>
+          </span>
+        </div>
       </div>
-      <div className='flex flex-col gap-y-2 py-12'>
-      <p>Home</p>
-      <p>About US</p>
-      <p>Customer Services</p>
-      <p>Products</p>
-      <p>Spaire Parts</p>
-      
-      <p>Blog</p>
-
+      <div className="flex flex-col items-center  ">
+        <p> © 2024, Trending Baby, All Rights Reserved</p>
       </div>
-
-      <div className='flex flex-col gap-y-2'>
-      <h1 className='font-medium text-4xl '> Socials</h1>
-  <span className='bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center'> 
-    <a href="" target="_blank" rel="noreferrer">
-      <FaFacebookF color="#5ac5f1" />
-    </a>
-   
-   </span>
-
-
-   <span className='bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center'> 
-    <a href="" target="_blank" rel="noreferrer">
-      <FaInstagram color="#5ac5f1" />
-    </a>
-     
-   </span>
-
-   <span className='bg-[#ecf0f3] rounded-full h-[25px] w-[25px] flex items-center justify-center'> 
-    <a href="" target="_blank" rel="noreferrer">
-      <FaTiktok color="#5ac5f1" />
-    </a>
-     
-   </span>
-  </div>
- 
-
-
     </div>
-    <div className='flex flex-col items-center  '>
-  <p> © 2024, Trending Baby, All Rights Reserved</p>
-  </div>
-    </div>
-  )
+  );
 }
-
---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  locales: ["en", "ar"],
+  defaultLocale: "en",
+  localeDetection: false,
+});
+
+export const config = {
+  // Skip all paths that should not be internationalized
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
